e3e04bc30e9a70dd47d0d4e7d55e90b0753de918
Quincy Sire <quincy.sire@student.moringaschool.com>
2024-04-07 16:06:16 +0300

    Completed Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print ("Hello, programmer!")
     pass
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
+pass
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default() 
+greet_with_default("Sunny")  
+pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+sum = add(1, 2)
+print(sum)
+pass
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    
+    return number / 2
+pass
